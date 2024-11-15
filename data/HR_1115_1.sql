197d122efbfe339e885f91aad8ed9e74555303c5
chan237 <bak7771@naver.com>
2024-11-15 11:41:31 +0900

    PLSQL EMPLOYEES TABLE에서 부서별로 사용자 정보(이름, 월급)를 CORSOR에 저장하고 각 해당하는 부서별 요청시 해당되는 부서정보를 출력하는 테스트

--- data/HR_1115_1.sql
+++ data/HR_1115_1.sql
@@ -45,3 +45,39 @@ END;
 --END LOOP;
 --3번째 방법
 --커서도 지워도 됨
+
+
+--EMPLOYEES TABLE에서 부서별로 사용자 정보(이름, 월급)를 CORSOR에 저장하고 각 해당하는 부서별 요청시 해당되는 부서정보를 출력하라
+
+DECLARE
+    VEMP_ROWTYPE EMPLOYEES%ROWTYPE;
+    VSALARY VARCHAR2(20);
+    VNO NUMBER(4);
+    --부서별로 정로를 저장할 수 있는 커서 생성
+   CURSOR C1(VDEPTNO EMPLOYEES.DEPARTMENT_ID%TYPE)
+   IS SELECT*FROM EMPLOYEES WHERE DEPARTMENT_ID = VDEPTNO;
+BEGIN
+    --부서별 정보를 생성(랜덤)
+    VNO := ROUND(DBMS_RANDOM.VALUE(10,110),-1);
+    --부서번호가 40번이면 종료
+    IF(VNO=40) THEN
+        DBMS_OUTPUT.PUT_LINE(VNO ||'해당하지 않는 부서번호');
+        RETURN;
+    END IF;
+    --부서별 정보를 가져와서 월급에 대해 평가 진행
+    FOR VEMP_ROWTYPE IN C1(VNO) LOOP
+        IF VEMP_ROWTYPE.SALARY BETWEEN 1 AND 3000 THEN
+            VSALARY := '낮음';
+        ELSIF VEMP_ROWTYPE.SALARY BETWEEN 3001 AND 5000 THEN
+            VSALARY := '중간';
+        ELSIF VEMP_ROWTYPE.SALARY BETWEEN 5001 AND 8000 THEN
+            VSALARY := '높음';
+        ELSE
+            VSALARY := '최고';
+        END IF;
+        DBMS_OUTPUT.PUT_LINE(VNO || ' / ' ||VEMP_ROWTYPE.FIRST_NAME || ' / ' || VEMP_ROWTYPE.SALARY || ' / ' || VSALARY);
+    END LOOP;
+END;
+/
+
+
